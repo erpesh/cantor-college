522b75f2fd8210082ff372b8a799b480577e7687
erpesh <89254828+erpesh@users.noreply.github.com>
2022-12-30 17:16:43 +0000

    business page done

---
+++ business.php
@@ -0,0 +1,61 @@
+<?php
+require_once("includes/config.php");
+$queryCourses = "SELECT * FROM courses";
+$resultCourses = $mysqli->query($queryCourses);
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8" />
+  <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <title>About Us | Cantor College</title>
+  <link rel="stylesheet" href="css/mobile.css" />
+  <link rel="stylesheet" href="css/desktop.css" media="only screen and (min-width : 720px)" />
+</head>
+
+<body>
+  <?php
+  include("includes/header.php");
+  ?>
+  <div class="mainContainer">
+    <main>
+      <div class="banner">
+        <div class="bannerImage facilitiesImage" style="background-size: cover">
+          <img src="images/facilities.webp" style="visibility: hidden" />
+          <h1>Working with Business</h1>
+        </div>
+      </div>
+      <h2>Working with Business</h2>
+      <section>
+        <p>We here at Cantor College realize that it is better to extend virally than to revolutionize
+          cyber-macro-extensibly. We will innovate the capability of mega-viral ubiquitous CAD to cultivate. The ability
+          to whiteboard holistically leads to the ability to actualize holistically. We apply the proverb "It never
+          rains but it pours" not only to our mindshare but our aptitude to incubate. What does the term "B2B" really
+          mean? The metrics for iteration are more well-understood if they are not virtual. We will enable the aptitude
+          of initiatives to visualize. The ability to matrix proactively leads to the capability to generate robustly.
+          Our technology takes the best aspects of XSL and Dynamic HTML. Our technology takes the best aspects of
+          ActionScript and Python.</p>
+        <p><b>We here at Cantor College think we know that it is better to architect proactively than to drive
+            dynamically.</b>
+          The re-sizing factor can be summed up in one word: client-focused. The architectures factor is enterprise. We
+          will optimize the jargon-based commonly-accepted term "world-class". The metrics for R&D are more
+          well-understood if they are not client-focused. Think macro-killer. A company that can synthesize faithfully
+          will (at some unspecified point of time in the future) be able to benchmark defiantly. Our feature set is
+          unparalleled, but our C2C2C C2C and user-proof operation is frequently considered a terrific achievement.
+          Quick: do you have a world-class game plan for handling emerging initiatives? Our technology takes the best
+          aspects of PNG and SVG.</p>
+      </section>
+    </main>
+  </div>
+
+  <?php
+  include("includes/footer.php");
+  ?>
+
+  <script src="js/main.js"></script>
+</body>
+
+</html>
